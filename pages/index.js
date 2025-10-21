27e34049dd790cab17d60d822594c426d921b744
Haniel <62188157+haniellourenco@users.noreply.github.com>
2025-10-21 16:11:54 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Olá mundo. Teste Vercel</h1>
+function Home() {
+  return <h1>Olá mundo. Teste Vercel</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
