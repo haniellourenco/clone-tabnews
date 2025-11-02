cf50b8e825d12bc6dfb07a5da23bbc9e6d048574
Haniel <62188157+haniellourenco@users.noreply.github.com>
2025-11-02 17:42:39 +0000

    add `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,15 +1,13 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
-  response
-    .status(200)
-    .json({
-      status: "ok",
-      timestamp: new Date().toISOString(),
-      db: result.rows[0],
-    });
+  response.status(200).json({
+    status: "ok",
+    timestamp: new Date().toISOString(),
+    db: result.rows[0],
+  });
 }
 
 export default status;
